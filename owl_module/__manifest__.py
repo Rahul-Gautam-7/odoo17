92a96b89e1bb70f180d99794bd401fc2665958c3
Rahul-Gautam-7 <143687411+Rahul-Gautam-7@users.noreply.github.com>
2025-03-29 13:27:10 +0530

    view_inheritance_using owljs

--- owl_module/__manifest__.py
+++ owl_module/__manifest__.py
@@ -6,11 +6,12 @@
     'author': 'Rahul',
     'license': 'LGPL-3',
     'depends': [
-      'web'  
+      'web','base',  
     ],
     'data': [
         'security/ir.model.access.csv',
         'views/todo_list.xml',
+        'views/res_partner.xml',
        
     ],
     'demo': [
@@ -22,7 +23,13 @@
         'web.assets_backend':[
             'owl_module/static/src/components/todo_list/todo_list.js',
             'owl_module/static/src/components/todo_list/todo_list.xml',
-           
+            'owl_module/static/src/components/view_inheritance/res_partner_list_view.js',
+            'owl_module/static/src/components/view_inheritance/res_partner_list_view.xml',
+            'owl_module/static/src/components/view_inheritance/res_partner_kanban_view.js',
+            'owl_module/static/src/components/view_inheritance/res_partner_form_view.js',
+            'owl_module/static/src/components/view_inheritance/res_partner_form_view.xml',
+            'owl_module/static/src/components/view_inheritance/res_partner_kanban_view.xml',
+            'owl_module/static/src/components/view_inheritance/res_partner_kanban_view.scss',   
         ]
     }
 }
